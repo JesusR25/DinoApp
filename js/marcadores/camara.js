615aa7e403a4cebdc4a2682d88c1ede1b1ce5703
Jesus Ramon Ramirez Medina <91562346+JesusR25@users.noreply.github.com>
2023-05-07 23:58:06 +0000

    Con rotacion

--- js/marcadores/camara.js
+++ js/marcadores/camara.js
@@ -161,3 +161,90 @@ function velo(){
   activo = true;
   modelo = 8;
 }
+
+//Controlador para movimientos
+AFRAME.registerComponent("movimiento", {
+  init: function () {
+      // track markerFound/markerLost
+      // grab the model reference
+      document.querySelector("#tmod").addEventListener("model-loaded", evt => {
+        modmega = evt.detail.model;
+          this.mesh = evt.detail.model
+      })
+      // hammerjs input helper
+      const hammertime = new Hammer(document.body);
+
+      // scale
+      // scale is tricky, because it resets
+      var currentScale = 1;
+      hammertime.get('pinch').set({ enable: true });
+      hammertime.on("pinchstart", (ev) => {
+          currentScale = this.mesh.scale.x;
+      })
+      hammertime.on("pinchmove", (ev) => {
+          if (!mega) return;
+          modmega.scale.multiplyScalar(0).addScalar(ev.scale * currentScale);
+      });
+
+      // rotation
+      // pan left/right for rotation
+      this.isPanning = false;
+      var xrot = false;
+      hammertime.on("panleft", () => {
+          if (!mega) return;
+          this.isPanning = true
+          modmega.rotation.y -= 4 * Math.PI / 360;
+      })
+
+      hammertime.on("panright", () => {
+          if (!mega) return;
+          this.isPanning = true
+          modmega.rotation.y += 4 * Math.PI / 360;
+      })
+
+      hammertime.on("panup", () => {
+          if (!mega) return;
+          xrot = true;
+          modmega.rotation.x -= 4 * Math.PI / 360;
+      })
+
+      hammertime.on("pandown", () => {
+          if (!mega) return;
+          xrot = true;
+          modmega.rotation.x += 4 * Math.PI / 360;
+      })
+
+
+      hammertime.on("panend", () => this.isPanning = false, xrot = false)
+      hammertime.on("pancancel", () => this.isPanning = false, xrot = false)
+
+      hammertime.on("swipeleft", ({ velocity }) => {
+          if (!mega) return;
+          this.swipeVelocity = velocity
+      })
+      hammertime.on("swiperight", ({ velocity }) => {
+          if (!mega) return;
+          this.swipeVelocity = velocity
+      })
+      hammertime.on("swipeup", ({ velocity }) => {
+          if (!mega) return;
+          this.swipeVelocity = velocity
+      })
+      hammertime.on("swipedown", ({ velocity }) => {
+          if (!mega) return;
+          this.swipeVelocity = velocity
+      })
+  },
+  tick: function () {
+      if (!(mega && this.swipeVelocity && !this.isPanning)){
+          return;
+      }else{
+          modmega.rotation.y += this.swipeVelocity * 4 * Math.PI / 360;
+          //this.mesh.rotation.x += this.swipeVelocity * 4 * Math.PI / 360;
+          this.swipeVelocity *= 0.93;
+          if (Math.abs(this.swipeVelocity) <= 0.1) this.swipeVelocity = 0;
+      }
+      
+      
+  }
+})
