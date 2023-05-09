445b3ab8b24a78bdd9776fa25b1c08d0b52cf72e
Jesus Ramon Ramirez Medina <91562346+JesusR25@users.noreply.github.com>
2023-05-09 14:11:07 +0000

    Con id

--- js/marcadores/camara.js
+++ js/marcadores/camara.js
@@ -1020,7 +1020,7 @@ function componentes(modelo) {
 }
 
 function f(){
-    html2canvas(document.querySelector("#cuerpo")).then(canvas => {
+    html2canvas(document.querySelector("#captura")).then(canvas => {
         return Canvas2Image.saveAsImage(canvas, null, null, "png", "img");
     });
 }
