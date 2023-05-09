a330ecfb78d3a253c6b0bd84e7e0fee6c79fbc89
Jesus Ramon Ramirez Medina <91562346+JesusR25@users.noreply.github.com>
2023-05-09 17:36:53 +0000

    Archivos separados

---
+++ js/marcadores/captura.js
@@ -0,0 +1,27 @@
+// Put variables in global scope to make them available to the browser console.
+const video = document.querySelector('video');
+const canvas = window.canvas = document.querySelector('canvas');
+canvas.width = 480;
+canvas.height = 360;
+
+function captura() {
+    navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    return Canvas2Image.saveAsImage(canvas, null, null, "png", "img");
+}
+
+const constraints = {
+  audio: false,
+  video: true
+};
+
+function handleSuccess(stream) {
+  window.stream = stream; // make stream available to browser console
+  video.srcObject = stream;
+}
+
+function handleError(error) {
+  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+}
