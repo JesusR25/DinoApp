fbf461d5764ace3b4b42c79458738d8955de5997
Jesus Ramon Ramirez Medina <91562346+JesusR25@users.noreply.github.com>
2023-05-09 17:23:21 +0000

    Cabvas2Image

--- js/marcadores/camara.js
+++ js/marcadores/camara.js
@@ -1019,8 +1019,30 @@ function componentes(modelo) {
   }
 }
 
-function f(){
-    html2canvas(document.querySelector("#captura")).then(canvas => {
-        return Canvas2Image.saveAsImage(canvas, null, null, "png", "img");
-    });
+// Put variables in global scope to make them available to the browser console.
+const video = document.querySelector('video');
+const canvas = window.canvas = document.querySelector('canvas');
+canvas.width = 480;
+canvas.height = 360;
+
+function captura() {
+    navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    return Canvas2Image.saveAsImage(canvas, null, null, "png", "img");
+}
+
+const constraints = {
+  audio: false,
+  video: true
+};
+
+function handleSuccess(stream) {
+  window.stream = stream; // make stream available to browser console
+  video.srcObject = stream;
+}
+
+function handleError(error) {
+  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
 }
