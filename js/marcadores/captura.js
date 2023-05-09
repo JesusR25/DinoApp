20b1e2fa820e45e17b7662d55f23504b569244cf
Jesus Ramon Ramirez Medina <91562346+JesusR25@users.noreply.github.com>
2023-05-09 19:48:55 +0000

    COn 2 bibliotecas

--- js/marcadores/captura.js
+++ js/marcadores/captura.js
@@ -1,29 +1,57 @@
-// Put variables in global scope to make them available to the browser console.
-const video = document.querySelector('video');
-const canvas = window.canvas = document.querySelector('canvas');
-canvas.width = 480;
-canvas.height = 360;
+function resizeCanvas(origCanvas, width, height) {
+	let resizedCanvas = document.createElement("canvas");
+	let resizedContext = resizedCanvas.getContext("2d");
 
-function captura() {
-    navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
-    let mediaStream = navigator.mediaDevices.getDisplayMedia({video:true});
-    video.srcObject = mediaStream;
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-    
+	resizedCanvas.height = height;
+	resizedCanvas.width = width;
+
+	resizedContext.drawImage(origCanvas, 0, 0, width, height);
+	return resizedCanvas.toDataURL();
 }
 
-const constraints = {
-  audio: false,
-  video: true
+function captura(){
+    let aScene = document
+		.querySelector("a-scene")
+		.components.screenshot.getCanvas("perspective");
+	let frame = captureVideoFrame("video", "png");
+	aScene = resizeCanvas(aScene, frame.width, frame.height);
+	frame = frame.dataUri;
+	mergeImages([frame, aScene]).then(b64 => {
+		let link = document.getElementById("download-link", "png");
+		link.setAttribute("download", "AR.png");
+		link.setAttribute("href", b64);
+		link.click();
+	});
+}
+
+function captureVideoFrame(video, format, width, height) {
+    if (typeof video === 'string') {
+        video = document.querySelector(video);
+    }
+
+    format = format || 'jpeg';
+
+    if (!video || (format !== 'png' && format !== 'jpeg')) {
+        return false;
+    }
+
+    var canvas = document.createElement("CANVAS");
+
+    canvas.width = width || video.videoWidth;
+    canvas.height = height || video.videoHeight;
+    canvas.getContext('2d').drawImage(video, 0, 0);
+    var dataUri = canvas.toDataURL('image/' + format);
+    var data = dataUri.split(',')[1];
+    var mimeType = dataUri.split(';')[0].slice(5)
+
+    var bytes = window.atob(data);
+    var buf = new ArrayBuffer(bytes.length);
+    var arr = new Uint8Array(buf);
+
+    for (var i = 0; i < bytes.length; i++) {
+        arr[i] = bytes.charCodeAt(i);
+    }
+
+    var blob = new Blob([ arr ], { type: mimeType });
+    return { blob: blob, dataUri: dataUri, format: format, width: canvas.width, height: canvas.height };
 };
-
-function handleSuccess(stream) {
-  window.stream = stream; // make stream available to browser console
-  video.srcObject = stream;
-}
-
-function handleError(error) {
-  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
-}
